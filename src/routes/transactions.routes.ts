9cb4312a4ddd9f807cf0ba7988142b4b7069e58f
robertoalvesferreira <robertoaff06@gmail.com>
2020-05-18 21:34:30 -0300

    Finalizando as migrations de Categorias e Transação, e iniciando post de transação

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,19 +1,28 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  response.json({ ok: 'message' });
   // TODO
 });
 
 transactionsRouter.post('/', async (request, response) => {
   // TODO
+  const { title, value, type } = request.body;
+  console.log(title, value, type);
+  const createTransactionService = new CreateTransactionService();
+
+  const transaction = await createTransactionService.execute({
+    title,
+    value,
+    type,
+  });
+  response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,28 @@
 // import AppError from '../errors/AppError';
-
+import { getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+// import Caregory from '../models/Category';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
+  public async execute({ title, value, type }: Request): Promise<Transaction> {
     // TODO
+    const transactionRepository = getRepository(Transaction);
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: 'a1dbdb2b-4d3e-4b3d-a7b6-8b0825638c16',
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
